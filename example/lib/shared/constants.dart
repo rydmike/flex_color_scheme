907433f80f2b6a32e36cd34774f11c987a4e946c
Mike Rydstrom <mrydstrom+gh@gmail.com>
2021-03-15 08:48:37 +0200

    Version nr update for Web examples

--- example/lib/shared/constants.dart
+++ example/lib/shared/constants.dart
@@ -12,7 +12,7 @@ class AppConst {
   // When I build a new public version I just make sure to update this info
   // before building them.
   static const String appName = 'FlexColorScheme';
-  static const String version = '2.0.0-nullsafety.2';
+  static const String version = '2.0.0';
   static const String packageVersion = 'FlexColorScheme package $version';
   static const String packageUrl = 'https://pub.dev/packages/flex_color_scheme';
   static const String flutterVersion = 'Channel dev, 2.1.0-12.1.pre, CanvasKit';
