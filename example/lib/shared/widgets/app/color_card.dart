c858f200cdf2be16fa73dda7f36cc487d0a2ace5
Mike Rydstrom <mrydstrom+gh@gmail.com>
2022-03-28 00:05:23 +0300

    Make ColorCard phone size responsive for tighter phone layout

--- example/lib/shared/widgets/app/color_card.dart
+++ example/lib/shared/widgets/app/color_card.dart
@@ -3,40 +3,45 @@ import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 
 import '../../../example5/widgets/shared/copy_color_to_clipboard.dart';
+import '../../const/app_data.dart';
 
 /// This is just simple SizedBox with a Card with a passed in label, background
 /// and text label color. Used to show the colors of a theme or scheme
 /// color property.
 ///
-/// It also uses a tooltip to shpow the color code, its Material name, if it
+/// It also uses a tooltip to show the color code, its Material name, if it
 /// is a Material 2 color, and iot always shows it general/common name, from
 /// a list of +1600 color names.
 ///
 /// When you tap the ColorCard the color value is copied to clipboard.
-
 class ColorCard extends StatelessWidget {
   const ColorCard({
     Key? key,
     required this.label,
     required this.color,
     required this.textColor,
-    this.size = const Size(80, 56),
+    this.size,
   }) : super(key: key);
 
   final String label;
   final Color color;
   final Color textColor;
-  final Size size;
+  final Size? size;
 
   @override
   Widget build(BuildContext context) {
     final String materialName = ColorTools.materialName(color);
     final String nameThatColor = ColorTools.nameThatColor(color);
     final String space = materialName == '' ? '' : ' ';
+    final MediaQueryData media = MediaQuery.of(context);
+    final bool isPhone = media.size.width < AppData.phoneBreakpoint;
+    final double fontSize = isPhone ? 10 : 11;
+    final Size effectiveSize =
+        size ?? (isPhone ? const Size(78, 54) : const Size(86, 58));
 
     return SizedBox(
-      width: size.width,
-      height: size.height,
+      width: effectiveSize.width,
+      height: effectiveSize.height,
       child: Tooltip(
         message: 'Color #${color.hexCode} $nameThatColor$space$materialName.'
             '\nTap box to copy RGB value to Clipboard.',
@@ -51,7 +56,7 @@ class ColorCard extends StatelessWidget {
             child: Center(
               child: Text(
                 label,
-                style: TextStyle(color: textColor, fontSize: 11),
+                style: TextStyle(color: textColor, fontSize: fontSize),
                 textAlign: TextAlign.center,
               ),
             ),
