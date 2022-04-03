f67b54d144605e1e8a7dd2f2193abfcda20ee8d9
Mike Rydstrom <mrydstrom+gh@gmail.com>
2022-04-04 02:09:48 +0300

    Added found Flutter SDK bug notice on UnderlineInputBorder

--- example/lib/example5/widgets/panels/text_field_settings/text_field_settings.dart
+++ example/lib/example5/widgets/panels/text_field_settings/text_field_settings.dart
@@ -13,6 +13,8 @@ class TextFieldSettings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final bool isLight = Theme.of(context).brightness == Brightness.light;
+    debugPrint('TextFieldSettings: inputDecoratorBorderType: '
+        '${controller.inputDecoratorBorderType}');
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
@@ -81,6 +83,14 @@ class TextFieldSettings extends StatelessWidget {
                 }
               : null,
         ),
+        // TODO(rydmike): Report Flutter bug on UnderlineInputBorder.
+        // When using UnderlineInputBorder the border radius does not update
+        // when ThemeData has changed to a new version with different
+        // underline input border radius. this is because UnderlineInputBorder
+        // does not implement correctly where as OutlineInputBorder does.
+        // Issue is here:
+        // https://github.com/flutter/flutter/blob/0b97874895eb3e14c1cf0e65bd7fca3a17c62b02/packages/flutter/lib/src/material/input_border.dart#L244
+        // It needs border radius as a part of its equality and hashcode.
         ListTile(
           enabled: controller.useSubThemes && controller.useFlexColorScheme,
           title: const Text('Border radius'),
