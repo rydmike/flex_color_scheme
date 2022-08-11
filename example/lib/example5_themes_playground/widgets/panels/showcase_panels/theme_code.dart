201f602d4495bf8ca62be7c2d77dcbaeb19a463c
Rydmike <m.rydstrom@gmail.com>
2022-08-12 02:20:39 +0300

    Themes Playground: Add CodeTheme and for it demo of M3 color harmonization

--- example/lib/example5_themes_playground/widgets/panels/showcase_panels/theme_code.dart
+++ example/lib/example5_themes_playground/widgets/panels/showcase_panels/theme_code.dart
@@ -5,7 +5,13 @@ import 'package:google_fonts/google_fonts.dart';
 import '../../../../shared/controllers/theme_controller.dart';
 import '../../../../shared/widgets/universal/syntax_highlighter.dart';
 import '../../../utils/generate_theme_dart_code.dart';
+import 'show_code_theme_colors.dart';
 
+/// Show the code for the currently configured FlexColorScheme theme setup.
+///
+/// Used by in a panel to show the code next to settings panels.
+/// As theme settings are configured and changed, the generated setup code
+/// is updated and can be copied to the clipboard at any point.
 class ThemeCode extends StatelessWidget {
   const ThemeCode(this.controller, {super.key});
   final ThemeController controller;
@@ -23,10 +29,6 @@ class ThemeCode extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final SyntaxHighlighterStyle style =
-        Theme.of(context).brightness == Brightness.dark
-            ? SyntaxHighlighterStyle.darkThemeStyle()
-            : SyntaxHighlighterStyle.lightThemeStyle();
     final String code = generateThemeDartCode(controller);
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -52,9 +54,32 @@ class ThemeCode extends StatelessWidget {
           child: SelectableText.rich(
             TextSpan(
               style: GoogleFonts.firaMono(fontSize: 12),
-              children: <TextSpan>[DartSyntaxHighlighter(style).format(code)],
+              children: <TextSpan>[
+                DartSyntaxHighlighter(
+                  SyntaxHighlighterStyle.dynamic(context),
+                ).format(code),
+              ],
             ),
           ),
+        ),
+        const Divider(height: 1),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: ShowCodeThemeColors(),
+        ),
+        const ListTile(
+          dense: true,
+          title: Text('CodeTheme is a ThemeData ThemeExtension'),
+          subtitle: Text('The above colors have been added as a custom theme '
+              'extension to ThemeData. Light and dark mode CodeTheme change '
+              'with theme mode and is accessed via Theme.of(context). The '
+              'extension colors are also using Material 3 color harmonization '
+              'with active theme. When you change theme you can see that '
+              'these semantic CodeTheme colors are often automatically '
+              'slightly adjusted towards the overall theme colors. '
+              'This CodeTheme is used as a ThemeExtension example in the '
+              'Themes Playground app. It also demonstrates how to use M3 color '
+              'harmonization of custom theme colors in Flutter.'),
         ),
         const SizedBox(height: 8),
       ],
