bb7d043d30427415bc09c91ba4bd966110e5ffc3
Rydmike <39990307+rydmike@users.noreply.github.com>
2023-01-17 06:48:10 +0200

    Change Playground to default to M3 mode

--- example/lib/shared/const/store.dart
+++ example/lib/shared/const/store.dart
@@ -228,7 +228,7 @@ class Store {
   // Key used to read and save the useMaterial3 value.
   static const String keyUseMaterial3 = 'useMaterial3';
   // Default value for the useMaterial3, also to reset settings.
-  static const bool defaultUseMaterial3 = false;
+  static const bool defaultUseMaterial3 = true;
 
   // Key used to read and save the useKeyColors value.
   static const String keyUseKeyColors = 'useKeyColors';
