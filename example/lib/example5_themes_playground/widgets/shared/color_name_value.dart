8a34a345de2c497b1eafc13d4ae09ff7002af27e
Rydmike <39990307+rydmike@users.noreply.github.com>
2023-03-07 14:39:24 +0200

    Playground: Improve switch track outline color on seeded colorscheme lock switches

--- example/lib/example5_themes_playground/widgets/shared/color_name_value.dart
+++ example/lib/example5_themes_playground/widgets/shared/color_name_value.dart
@@ -277,6 +277,18 @@ class _ColorNameValueState extends State<ColorNameValue> {
                       widget.color.withAlpha(0xAA), widget.textColor),
                   inactiveThumbColor: Color.alphaBlend(
                       widget.color.withAlpha(0xCC), widget.textColor),
+                  trackOutlineColor: theme.useMaterial3
+                      ? MaterialStateProperty.resolveWith(
+                          (Set<MaterialState> states) {
+                          if (states.contains(MaterialState.selected)) {
+                            return Colors.transparent;
+                          }
+                          if (states.contains(MaterialState.disabled)) {
+                            return theme.colorScheme.onSurface.withAlpha(0x1F);
+                          }
+                          return widget.textColor.withAlpha(0x26);
+                        })
+                      : null,
                   onChanged: widget.onLocked?.call,
                   // materialTapTargetSize: MaterialTapTargetSize.padded,
                 ),
