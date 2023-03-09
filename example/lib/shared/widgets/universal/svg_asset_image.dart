ef9394c87bfdb9420cabeb8b03532ee1846153a8
Rydmike <39990307+rydmike@users.noreply.github.com>
2023-03-09 18:28:52 +0200

    Update svg_asset_image.dart

--- example/lib/shared/widgets/universal/svg_asset_image.dart
+++ example/lib/shared/widgets/universal/svg_asset_image.dart
@@ -1,19 +1,19 @@
 import 'dart:async' show Future;
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 // ignore_for_file: prefer_asserts_with_message
 
-/// Displays an SVG image provided in the applications asset bundle.
+/// Displays an Undraw SVG image provided in the applications asset bundle.
 ///
 /// The [assetName] is the path and name of the SVG file in the asset bundle.
 /// This SVG asset display widget is tuned for usage with the Undraw images
 /// by Katerina Limpitsouni found at https://undraw.co/
-/// The [color] property can be used to change the main color theme in the
+/// The [color] property can be used to change the main color theme in the SVG
 /// drawing and thus theme the image to fit with colors used in the application.
-class SvgAssetImage extends StatelessWidget {
+class SvgAssetImage extends StatefulWidget {
   const SvgAssetImage({
     super.key,
     required this.assetName,
@@ -39,54 +39,66 @@ class SvgAssetImage extends StatelessWidget {
   final Widget? errorWidget;
   final EdgeInsets? padding;
 
-  Future<SvgPicture> renderIllustration(
-      final String name, final Color changeColor) async {
-    String image = await loadAsset();
-    String valueString = changeColor.toString().split('(0x')[1].split(')')[0];
+  @override
+  State<SvgAssetImage> createState() => _SvgAssetImageState();
+}
+
+class _SvgAssetImageState extends State<SvgAssetImage> {
+  late final Future<SvgPicture> svgImage;
+
+  Future<SvgPicture> _coloredUndrawImage(
+      final String name, final Color color) async {
+    String image = await rootBundle.loadString(widget.assetName);
+    String valueString = color.toString().split('(0x')[1].split(')')[0];
     valueString = valueString.substring(2, valueString.length);
 
     // Find the default image 'theme' color in the Undraw SVG, and replace
     // the color with another color string value we want to use instead.
     image = image.replaceAll('#6c63ff', '#$valueString');
     return SvgPicture.string(
+      key: ValueKey<String>('${widget.assetName}$color'),
       image,
-      height: height,
-      width: width,
-      alignment: alignment,
-      fit: fit,
-      semanticsLabel: semanticLabel,
+      height: widget.height,
+      width: widget.width,
+      alignment: widget.alignment,
+      fit: widget.fit,
+      semanticsLabel: widget.semanticLabel,
     );
   }
 
-  Future<String> loadAsset() async {
-    return rootBundle.loadString(assetName);
+  @override
+  void initState() {
+    super.initState();
+    // False positive, see: https://github.com/dart-lang/linter/issues/3739
+    // ignore: discarded_futures
+    svgImage = _coloredUndrawImage(widget.assetName, widget.color);
   }
 
   @override
   Widget build(final BuildContext context) {
     return FutureBuilder<SvgPicture>(
-      // ignore: discarded_futures, this seems to be a bug in the linter
-      future: renderIllustration(assetName, color),
+      future: svgImage,
       builder:
           (final BuildContext context, final AsyncSnapshot<Widget> snapshot) {
         if (snapshot.hasData) {
           return Padding(
-            padding: padding ?? const EdgeInsets.all(16),
+            padding: widget.padding ?? const EdgeInsets.all(16),
             child: snapshot.data,
           );
         } else if (snapshot.hasError) {
           return ConstrainedBox(
             constraints: BoxConstraints.tightFor(
-              height: height,
-              width: width,
+              height: widget.height,
+              width: widget.width,
             ),
             child: Center(
-              child: errorWidget ?? const Text('Could not load asset image!'),
+              child: widget.errorWidget ??
+                  const Text('Could not load asset image!'),
             ),
           );
         } else {
           return Center(
-            child: placeholder ?? const CircularProgressIndicator(),
+            child: widget.placeholder ?? const CircularProgressIndicator(),
           );
         }
       },
