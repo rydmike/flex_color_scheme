38ed3c490b255dfb7aa9c7699dcd9a0038d14f15
Rydmike <39990307+rydmike@users.noreply.github.com>
2023-03-20 21:42:53 +0200

    TEST: FlexSubThemes navigationDrawerTheme -> 84%

--- test/flex_sub_themes_test.dart
+++ test/flex_sub_themes_test.dart
@@ -4577,8 +4577,101 @@ void main() {
     // -------------------------------------------------------------------------
     test(
         'NavigationDrawer FST22.1: GIVEN a FlexSubTheme.navigationDrawerTheme '
-        'EXPECT equal to ...',
-        () {});
+        'EXPECT equal to NavigationDrawerThemeData with same values', () {
+      final ColorScheme colorScheme = ColorScheme.fromSeed(
+        seedColor: const Color(0xFF79E742),
+        brightness: Brightness.light,
+      );
+      NavigationDrawerThemeData m = FlexSubThemes.navigationDrawerTheme(
+        colorScheme: colorScheme,
+      );
+      expect(
+        m.backgroundColor,
+        equals(colorScheme.surface),
+      );
+      expect(
+        m.indicatorColor,
+        equals(colorScheme.secondaryContainer),
+      );
+      expect(
+        m.indicatorSize,
+        equals(const Size(336, 56)),
+      );
+      expect(
+        m.labelTextStyle!
+            .resolve(<MaterialState>{MaterialState.selected})?.color,
+        equals(colorScheme.onSecondaryContainer),
+      );
+      expect(
+        m.labelTextStyle!.resolve(<MaterialState>{})?.color,
+        equals(colorScheme.onSurface),
+      );
+      expect(
+        m.iconTheme!.resolve(<MaterialState>{MaterialState.selected})?.color,
+        equals(colorScheme.onSecondaryContainer),
+      );
+      expect(
+        m.iconTheme!.resolve(<MaterialState>{})?.color,
+        equals(colorScheme.onSurface),
+      );
+      m = FlexSubThemes.navigationDrawerTheme(
+        colorScheme: colorScheme,
+        backgroundSchemeColor: SchemeColor.secondaryContainer,
+        indicatorSchemeColor: SchemeColor.primary,
+        selectedItemSchemeColor: SchemeColor.secondaryContainer,
+        unselectedItemSchemeColor: SchemeColor.onTertiaryContainer,
+        surfaceTintColor: colorScheme.tertiary,
+        shadowColor: Colors.red,
+        indicatorOpacity: 0.95,
+        indicatorRadius: 8,
+        indicatorWidth: 304,
+      );
+      expect(
+        m.backgroundColor,
+        equals(colorScheme.secondaryContainer),
+      );
+      expect(
+        m.surfaceTintColor,
+        equals(colorScheme.tertiary),
+      );
+      expect(
+        m.shadowColor,
+        equals(Colors.red),
+      );
+      expect(
+        m.indicatorColor,
+        equals(colorScheme.primary.withOpacity(0.95)),
+      );
+      expect(
+        m.indicatorSize,
+        equals(const Size(304, 56)),
+      );
+      expect(
+        m.labelTextStyle!
+            .resolve(<MaterialState>{MaterialState.selected})?.color,
+        equals(colorScheme.secondaryContainer),
+      );
+      expect(
+        m.labelTextStyle!.resolve(<MaterialState>{})?.color,
+        equals(colorScheme.onTertiaryContainer),
+      );
+      expect(
+        m.iconTheme!.resolve(<MaterialState>{MaterialState.selected})?.color,
+        equals(colorScheme.secondaryContainer),
+      );
+      expect(
+        m.iconTheme!.resolve(<MaterialState>{})?.color,
+        equals(colorScheme.onTertiaryContainer),
+      );
+      expect(
+        m.indicatorShape,
+        equals(const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(8),
+          ),
+        )),
+      );
+    });
   });
   group('WITH: FlexSubTheme.navigationRailTheme ', () {
     // -------------------------------------------------------------------------
