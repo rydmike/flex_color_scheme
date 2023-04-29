339cb491e55f702212068eefe67bc095a609fd0b
Rydmike <39990307+rydmike@users.noreply.github.com>
2023-04-29 15:39:22 +0300

    Update app.dart

--- example/lib/shared/const/app.dart
+++ example/lib/shared/const/app.dart
@@ -41,7 +41,7 @@ class App {
   // build numbers.
   static const String versionMajor = '7';
   static const String versionMinor = '0';
-  static const String versionPatch = '4';
+  static const String versionPatch = '5';
   static const String versionBuild = '01';
   static const String version = '$versionMajor.$versionMinor.$versionPatch '
       'Build-$versionBuild';
