e54b703789a3142bc9a01ea932ed64bd1539351e
Rydmike <39990307+rydmike@users.noreply.github.com>
2023-07-04 18:58:38 +0300

    Update Flutter version info

--- example/lib/shared/const/app.dart
+++ example/lib/shared/const/app.dart
@@ -49,7 +49,7 @@ class App {
   static const String packageVersion =
       '$versionMajor.$versionMinor.$versionPatch';
   static const String packageVersionMinor = '$versionMajor.$versionMinor.x';
-  static const String flutterVersion = 'stable 3.10.1 (canvaskit)';
+  static const String flutterVersion = 'stable 3.10.5 (canvaskit)';
   static const String copyright = '© 2020 - 2023';
   static const String author = 'Mike Rydstrom';
   static const String license = 'BSD 3-Clause License';
