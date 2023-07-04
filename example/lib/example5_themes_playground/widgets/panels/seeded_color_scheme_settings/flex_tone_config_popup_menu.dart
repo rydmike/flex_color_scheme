d631db3a5dfa27fa133b86455b1644cd1b277ae7
Rydmike <39990307+rydmike@users.noreply.github.com>
2023-07-05 00:59:31 +0300

    Rename FlexTone to FlexTonesEnum

--- example/lib/example5_themes_playground/widgets/panels/seeded_color_scheme_settings/flex_tone_config_popup_menu.dart
+++ example/lib/example5_themes_playground/widgets/panels/seeded_color_scheme_settings/flex_tone_config_popup_menu.dart
@@ -7,7 +7,7 @@ import '../../shared/color_scheme_box.dart';
 
 /// Widget used to select used [FlexTones] with a popup menu.
 ///
-/// Uses enhanced enum [FlexTone] to get data for the UI.
+/// Uses enhanced enum [FlexTonesEnum] to get data for the UI.
 class FlexToneConfigPopupMenu extends StatelessWidget {
   const FlexToneConfigPopupMenu({
     super.key,
@@ -31,41 +31,42 @@ class FlexToneConfigPopupMenu extends StatelessWidget {
     final ColorScheme colorScheme = theme.colorScheme;
     final TextStyle txtStyle = theme.textTheme.labelLarge!;
     // Value less than 1, or index over range disables the control.
-    final bool disabled = index < 1 || index >= FlexTone.values.length;
+    final bool disabled = index < 1 || index >= FlexTonesEnum.values.length;
 
     return PopupMenuButton<int>(
       initialValue: disabled ? 1 : index,
       tooltip: '',
       padding: EdgeInsets.zero,
       onSelected: (int index) {
-        onChanged
-            ?.call(index >= FlexTone.values.length || index < 1 ? 0 : index);
+        onChanged?.call(
+            index >= FlexTonesEnum.values.length || index < 1 ? 0 : index);
       },
       enabled: !disabled,
       itemBuilder: (BuildContext context) => <PopupMenuItem<int>>[
         // The '0' index is excluded, it describes the disabled state.
-        for (int i = 1; i < FlexTone.values.length; i++)
+        for (int i = 1; i < FlexTonesEnum.values.length; i++)
           PopupMenuItem<int>(
             value: i,
             child: ListTile(
               dense: true,
               leading: ColorSchemeBox(
-                optionIcon: FlexTone.values[i].icon,
-                backgroundColor: FlexTone.values[i].shade < 0
-                    ? colorScheme.primary.lighten(FlexTone.values[i].shade * -1)
-                    : colorScheme.primary.darken(FlexTone.values[i].shade),
+                optionIcon: FlexTonesEnum.values[i].icon,
+                backgroundColor: FlexTonesEnum.values[i].shade < 0
+                    ? colorScheme.primary
+                        .lighten(FlexTonesEnum.values[i].shade * -1)
+                    : colorScheme.primary.darken(FlexTonesEnum.values[i].shade),
                 borderColor: Colors.transparent,
               ),
-              title: Text(FlexTone.values[i].toneLabel, style: txtStyle),
+              title: Text(FlexTonesEnum.values[i].toneLabel, style: txtStyle),
             ),
           )
       ],
       child: ListTileReveal(
         enabled: !disabled,
         contentPadding: contentPadding,
-        title: Text('$title ${FlexTone.values[index].toneLabel}'),
+        title: Text('$title ${FlexTonesEnum.values[index].toneLabel}'),
         subtitle: Text(
-          '${FlexTone.values[index].describe}.\n'
+          '${FlexTonesEnum.values[index].describe}.\n'
           '$flexToneName FlexTones setup has CAM16 chroma:\n'
           '\n'
           '$flexToneSetup\n'
@@ -76,7 +77,7 @@ class FlexToneConfigPopupMenu extends StatelessWidget {
           'for the three colors used as keys for primary, '
           'secondary and tertiary TonalPalettes. '
           'Here you can choose between the default Material-3 tone '
-          'mapping, plus eight pre-defined custom FlexTones setups. With '
+          'mapping, plus ten other pre-defined custom FlexTones setups. With '
           'the API you can make your own FlexTones configurations.\n',
         ),
         trailing: Padding(
@@ -87,7 +88,7 @@ class FlexToneConfigPopupMenu extends StatelessWidget {
             foregroundColor: disabled ? theme.dividerColor : null,
             borderColor: disabled ? theme.dividerColor : Colors.transparent,
             defaultOption: disabled,
-            optionIcon: FlexTone.values[index].icon,
+            optionIcon: FlexTonesEnum.values[index].icon,
           ),
         ),
       ),
