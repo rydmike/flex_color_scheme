592b0fcbd188fd73908eefeea7d564e87ea51342
Rydmike <39990307+rydmike@users.noreply.github.com>
2024-08-29 03:48:17 +0300

    Playground: Refactor BottomNavigationBarSettings to use SliderListTileReveal

--- example/lib/example5_themes_playground/widgets/panels/bottom_navigation_bar_settings/bottom_navigation_bar_settings.dart
+++ example/lib/example5_themes_playground/widgets/panels/bottom_navigation_bar_settings/bottom_navigation_bar_settings.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import '../../../../shared/controllers/theme_controller.dart';
 import '../../../../shared/widgets/universal/list_tile_reveal.dart';
 import '../../../../shared/widgets/universal/showcase_material.dart';
+import '../../../../shared/widgets/universal/slider_list_tile_reveal.dart';
 import '../../../../shared/widgets/universal/switch_list_tile_reveal.dart';
 import '../../shared/color_scheme_popup_menu.dart';
 
@@ -16,29 +17,20 @@ class BottomNavigationBarSettings extends StatelessWidget {
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
     final bool isDark = theme.brightness == Brightness.dark;
+
+    // The most common logic for enabling Playground controls.
+    final bool enableControl =
+        controller.useSubThemes && controller.useFlexColorScheme;
+
     final String labelForDefaultSelectedItem = isDark &&
             (!controller.useFlexColorScheme ||
                 (controller.bottomNavBarSelectedSchemeColor == null &&
                     controller.bottomNavBarUnselectedSchemeColor == null))
         ? 'default (secondary)'
         : 'default (primary)';
-    final bool muteUnselectedEnabled =
-        controller.useSubThemes && controller.useFlexColorScheme;
-    final String labelForDefaultUnelectedItem =
-        controller.useSubThemes && controller.useFlexColorScheme
-            ? 'default (onSurface)'
-            : 'default (ThemeData.unselectedWidgetColor)';
-    final bool navBarOpacityEnabled =
-        controller.useSubThemes && controller.useFlexColorScheme;
-    final double navBarOpacity = navBarOpacityEnabled
-        ? (controller.bottomNavigationBarOpacity ?? -0.01)
-        : -0.01;
-
-    // Logic for default elevation label.
-    final String elevationDefaultLabel =
-        controller.bottomNavigationBarElevation == null
-            ? 'default 3'
-            : controller.bottomNavigationBarElevation!.toStringAsFixed(1);
+    final String labelForDefaultUnelectedItem = enableControl
+        ? 'default (onSurface)'
+        : 'default (ThemeData.unselectedWidgetColor)';
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -66,12 +58,11 @@ class BottomNavigationBarSettings extends StatelessWidget {
         const SizedBox(height: 8),
         ColorSchemePopupMenu(
           title: const Text('Background color'),
-          labelForDefault:
-              controller.useSubThemes && controller.useFlexColorScheme
-                  ? 'default (surface)'
-                  : 'default (ThemeData.canvasColor)',
+          labelForDefault: enableControl
+              ? 'default (surface)'
+              : 'default (ThemeData.canvasColor)',
           index: controller.bottomNavBarBackgroundSchemeColor?.index ?? -1,
-          onChanged: controller.useSubThemes && controller.useFlexColorScheme
+          onChanged: enableControl
               ? (int index) {
                   if (index < 0 || index >= SchemeColor.values.length) {
                     controller.setBottomNavBarBackgroundSchemeColor(null);
@@ -82,107 +73,39 @@ class BottomNavigationBarSettings extends StatelessWidget {
                 }
               : null,
         ),
-        ListTile(
-          enabled: navBarOpacityEnabled,
-          title: const Text('Background opacity'),
-          subtitle: Slider(
-            min: -1,
-            max: 100,
-            divisions: 101,
-            label: navBarOpacityEnabled
-                ? controller.bottomNavigationBarOpacity == null ||
-                        (controller.bottomNavigationBarOpacity ?? -1) < 0
-                    ? 'default 100%'
-                    : (navBarOpacity * 100).toStringAsFixed(0)
-                : 'default 100%',
-            value: navBarOpacity * 100,
-            onChanged: navBarOpacityEnabled
-                ? (double value) {
-                    controller.setBottomNavigationBarOpacity(
-                        value < 0 ? null : value / 100);
-                  }
-                : null,
-          ),
-          trailing: Padding(
-            padding: const EdgeInsetsDirectional.only(end: 5),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'OPACITY',
-                  style: theme.textTheme.bodySmall,
-                ),
-                Text(
-                  navBarOpacityEnabled
-                      ? controller.bottomNavigationBarOpacity == null ||
-                              (controller.bottomNavigationBarOpacity ?? -1) < 0
-                          ? 'default 100%'
-                          : '${(navBarOpacity * 100).toStringAsFixed(0)} %'
-                      : 'default 100%',
-                  style: theme.textTheme.bodySmall!
-                      .copyWith(fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-          ),
+        SliderListTileReveal(
+          enabled: enableControl,
+          title: const Text('Opacity'),
+          value: controller.bottomNavigationBarOpacity,
+          onChanged: controller.setBottomNavigationBarOpacity,
+          min: 0,
+          max: 1,
+          divisions: 100,
+          valueDisplayScale: 100,
+          valueDecimalPlaces: 0,
+          valueHeading: 'OPACITY',
+          valueUnitLabel: ' %',
+          valueDefaultLabel: '100 %',
         ),
-        ListTile(
-          enabled: controller.useSubThemes && controller.useFlexColorScheme,
+        SliderListTileReveal(
+          enabled: enableControl,
           title: const Text('Elevation'),
-          subtitle: Slider(
-            min: -1,
-            max: 24,
-            divisions: 25,
-            label: controller.useSubThemes && controller.useFlexColorScheme
-                ? controller.bottomNavigationBarElevation == null ||
-                        (controller.bottomNavigationBarElevation ?? -1) < 0
-                    ? elevationDefaultLabel
-                    : (controller.bottomNavigationBarElevation
-                            ?.toStringAsFixed(1) ??
-                        '')
-                : 'default 8',
-            value: controller.useSubThemes && controller.useFlexColorScheme
-                ? controller.bottomNavigationBarElevation ?? -1
-                : -1,
-            onChanged: controller.useSubThemes && controller.useFlexColorScheme
-                ? (double value) {
-                    controller.setBottomNavigationBarElevation(
-                        value < 0 ? null : value.roundToDouble());
-                  }
-                : null,
-          ),
-          trailing: Padding(
-            padding: const EdgeInsetsDirectional.only(end: 5),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'ELEV',
-                  style: theme.textTheme.bodySmall,
-                ),
-                Text(
-                  controller.useSubThemes && controller.useFlexColorScheme
-                      ? controller.bottomNavigationBarElevation == null ||
-                              (controller.bottomNavigationBarElevation ?? -1) <
-                                  0
-                          ? elevationDefaultLabel
-                          : (controller.bottomNavigationBarElevation
-                                  ?.toStringAsFixed(1) ??
-                              '')
-                      : 'default 8',
-                  style: theme.textTheme.bodySmall!
-                      .copyWith(fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-          ),
+          value: controller.bottomNavigationBarElevation,
+          onChanged: controller.setBottomNavigationBarElevation,
+          min: 0,
+          max: 24,
+          divisions: 24,
+          valueHeading: 'ELEV',
+          valueDecimalPlaces: 1,
+          valueDefaultLabel: '3',
+          valueDefaultDisabledLabel: '8',
         ),
         ColorSchemePopupMenu(
           title: const Text('Selected item color'),
           subtitle: const Text('Label and icon, but own properties in API'),
           labelForDefault: labelForDefaultSelectedItem,
           index: controller.bottomNavBarSelectedSchemeColor?.index ?? -1,
-          onChanged: controller.useSubThemes && controller.useFlexColorScheme
+          onChanged: enableControl
               ? (int index) {
                   if (index < 0 || index >= SchemeColor.values.length) {
                     controller.setBottomNavBarSelectedSchemeColor(null);
@@ -198,7 +121,7 @@ class BottomNavigationBarSettings extends StatelessWidget {
           subtitle: const Text('Label and icon, but own properties in API'),
           labelForDefault: labelForDefaultUnelectedItem,
           index: controller.bottomNavBarUnselectedSchemeColor?.index ?? -1,
-          onChanged: controller.useSubThemes && controller.useFlexColorScheme
+          onChanged: enableControl
               ? (int index) {
                   if (index < 0 || index >= SchemeColor.values.length) {
                     controller.setBottomNavBarUnselectedSchemeColor(null);
@@ -215,27 +138,22 @@ class BottomNavigationBarSettings extends StatelessWidget {
               const Text('Unselected icon and text are less bright. '
                   'Shared setting for icon and text in the Playground, but '
                   'separate properties are used by the API.\n'),
-          value: muteUnselectedEnabled
+          value: enableControl
               ? controller.bottomNavBarMuteUnselected
-              : !muteUnselectedEnabled,
-          onChanged: muteUnselectedEnabled
-              ? controller.setBottomNavBarMuteUnselected
-              : null,
+              : !enableControl,
+          onChanged:
+              enableControl ? controller.setBottomNavBarMuteUnselected : null,
         ),
         SwitchListTile(
           title: const Text('Show selected labels'),
-          value: controller.bottomNavShowSelectedLabels &&
-              controller.useSubThemes &&
-              controller.useFlexColorScheme,
+          value: enableControl && controller.bottomNavShowSelectedLabels,
           onChanged: controller.useSubThemes && controller.useFlexColorScheme
               ? controller.setBottomNavShowSelectedLabels
               : null,
         ),
         SwitchListTile(
           title: const Text('Show unselected labels'),
-          value: controller.bottomNavShowUnselectedLabels &&
-              controller.useSubThemes &&
-              controller.useFlexColorScheme,
+          value: enableControl && controller.bottomNavShowUnselectedLabels,
           onChanged: controller.useSubThemes && controller.useFlexColorScheme
               ? controller.setBottomNavShowUnselectedLabels
               : null,
