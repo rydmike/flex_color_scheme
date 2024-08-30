8b15bdf73fdd76b9b55e01b60f4f9f5ebf5c87b3
Rydmike <39990307+rydmike@users.noreply.github.com>
2024-08-30 14:55:21 +0300

    Remove not used prop from popupLabelDefault from ColorSchemePopupMenu
    Remove old experrment, keep the Anchor one around still-

--- example/lib/example5_themes_playground/widgets/shared/color_scheme_popup_menu.dart
+++ example/lib/example5_themes_playground/widgets/shared/color_scheme_popup_menu.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 
 import 'color_scheme_box.dart';
 
+// TODO(rydmike): Refactor to be more like EnumPopupMenu.
+
 /// Widget used to select a ColorScheme based colors in example 5.
 ///
 /// Uses index out out of range of [SchemeColor] to represent and select
@@ -21,7 +23,7 @@ class ColorSchemePopupMenu extends StatelessWidget {
     this.colorPrefix = '',
     this.contentPadding,
     this.labelForDefault = 'default (primary)',
-    this.popupLabelDefault,
+    // this.popupLabelDefault,
   });
   final int index;
   final ValueChanged<int>? onChanged;
@@ -30,7 +32,7 @@ class ColorSchemePopupMenu extends StatelessWidget {
   final String colorPrefix;
   final EdgeInsetsGeometry? contentPadding; // Defaults to 16.
   final String labelForDefault;
-  final String? popupLabelDefault;
+  // final String? popupLabelDefault;
 
   @override
   Widget build(BuildContext context) {
@@ -92,8 +94,7 @@ class ColorSchemePopupMenu extends StatelessWidget {
                   defaultOption: i <= 0,
                 ),
                 title: i <= 0
-                    ? Text(popupLabelDefault ?? labelForDefault,
-                        style: txtStyle)
+                    ? Text(labelForDefault, style: txtStyle)
                     : Text(SchemeColor.values[i - 1].name, style: txtStyle),
               ),
             )
@@ -131,133 +132,9 @@ class ColorSchemePopupMenu extends StatelessWidget {
   }
 }
 
-// An experiment with popup menu on the icon instead of the ListTile..
+// OLD Experimental new version of the ColorSchemePopupMenu.
+// 24.8.2024
 //
-// Not actively used, it was a test to see if it would be a better solution.
-// TODO(rydmike): Remove ColorSchemePopupMenuOnIconTest if not used later.
-//
-/// Widget used to select a ColorScheme based colors in example 5.
-///
-/// Uses index out out of range of [SchemeColor] to represent and select
-/// no selection of [SchemeColor] which sets its value to null in parent,
-/// so we can use a selectable item as null input, to represent default value
-/// via no value definition. A bit ugly/pragmatic approach.
-class ColorSchemePopupMenuOnIconTest extends StatelessWidget {
-  const ColorSchemePopupMenuOnIconTest({
-    super.key,
-    required this.index,
-    this.onChanged,
-    this.title,
-    this.subtitle,
-    this.colorPrefix = '',
-    this.contentPadding,
-    this.labelForDefault = 'default (primary)',
-    this.popupLabelDefault,
-  });
-  final int index;
-  final ValueChanged<int>? onChanged;
-  final Widget? title;
-  final Widget? subtitle;
-  final String colorPrefix;
-  final EdgeInsetsGeometry? contentPadding; // Defaults to 16.
-  final String labelForDefault;
-  final String? popupLabelDefault;
-
-  @override
-  Widget build(BuildContext context) {
-    final ThemeData theme = Theme.of(context);
-    final ColorScheme colorScheme = theme.colorScheme;
-    final TextStyle txtStyle = theme.textTheme.labelMedium!;
-    final bool enabled = onChanged != null;
-    final bool useDefault = index < 0 || index >= SchemeColor.values.length;
-    final String colorName = enabled && !useDefault
-        ? SchemeColor.values[index].name
-        : labelForDefault;
-
-    return ListTile(
-        enabled: enabled,
-        contentPadding: contentPadding,
-        title: title,
-        subtitle: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text('Color $colorPrefix$colorName'),
-            if (subtitle != null) subtitle!,
-          ],
-        ),
-        trailing: PopupMenuButton<int>(
-          // useRootNavigator: true,
-          position: PopupMenuPosition.under,
-          offset: const Offset(0, -6),
-          constraints: const BoxConstraints(
-            minWidth: 260,
-            maxWidth: 260,
-            maxHeight: 540,
-          ),
-          popUpAnimationStyle: AnimationStyle.noAnimation,
-          initialValue: useDefault ? SchemeColor.values.length : index,
-          tooltip: '',
-          padding: EdgeInsets.zero,
-          onSelected: (int index) {
-            onChanged?.call(index >= SchemeColor.values.length ? -1 : index);
-          },
-          enabled: enabled,
-          itemBuilder: (BuildContext context) => <PopupMenuItem<int>>[
-            for (int i = 0; i <= SchemeColor.values.length; i++)
-              PopupMenuItem<int>(
-                value: i,
-                child: ListTile(
-                  dense: true,
-                  contentPadding: EdgeInsets.zero,
-                  leading: ColorSchemeBox(
-                    foregroundColor: i >= SchemeColor.values.length
-                        ? colorScheme.onSurface
-                        : FlexSubThemes.schemeColor(
-                            FlexSubThemes.onSchemeColor(SchemeColor.values[i]),
-                            colorScheme),
-                    backgroundColor: i >= SchemeColor.values.length
-                        ? colorScheme.surface
-                        : FlexSubThemes.schemeColor(
-                            SchemeColor.values[i],
-                            colorScheme,
-                          ),
-                    selected: i == index ||
-                        (index < 0 && i == SchemeColor.values.length),
-                    borderColor: i == index ||
-                            (index < 0 && i == SchemeColor.values.length)
-                        ? theme.colorScheme.onSurface
-                        : theme.dividerColor,
-                    defaultOption: i >= SchemeColor.values.length,
-                  ),
-                  title: i >= SchemeColor.values.length
-                      // If we are over enum length, make default label.
-                      ? Text(popupLabelDefault ?? labelForDefault,
-                          style: txtStyle)
-                      : Text(SchemeColor.values[i].name, style: txtStyle),
-                ),
-              )
-          ],
-          child: Padding(
-            padding: const EdgeInsetsDirectional.only(end: 5.0),
-            child: ColorSchemeBox(
-              foregroundColor: enabled && !useDefault
-                  ? FlexSubThemes.schemeColor(
-                      FlexSubThemes.onSchemeColor(SchemeColor.values[index]),
-                      colorScheme)
-                  : colorScheme.onSurface,
-              backgroundColor: enabled && !useDefault
-                  ? FlexSubThemes.schemeColor(
-                      SchemeColor.values[index], colorScheme)
-                  : colorScheme.surface,
-              borderColor: theme.dividerColor,
-              defaultOption: useDefault,
-            ),
-          ),
-        ));
-  }
-}
-
-// Experimental new version of the ColorSchemePopupMenu.
 // This is MenuAnchor based so it can open from tap point.
 // It reopens in new position if tapped again.
 // For now only used as a test on Buttons panel on ElevatedButton color.
