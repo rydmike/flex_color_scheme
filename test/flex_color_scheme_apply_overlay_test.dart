661afa584a5e32ac4f4856e189e7bf7f4f9165ec
Rydmike <39990307+rydmike@users.noreply.github.com>
2024-09-28 20:42:16 +0300

    TEST: Fix M2 dark mode overlay test to not use background color

--- test/flex_color_scheme_apply_overlay_test.dart
+++ test/flex_color_scheme_apply_overlay_test.dart
@@ -28,35 +28,36 @@ void main() {
       );
     }
 
-    ThemeData lightTheme(int surfaceBlend, int backgroundBlend) =>
-        ThemeData.from(
+    ThemeData lightTheme(int blend) => ThemeData.from(
           colorScheme: ColorScheme.light(
-            surface: Color.alphaBlend(
-                const ColorScheme.light().primary.withAlpha(surfaceBlend),
+            surfaceContainer: Color.alphaBlend(
+                const ColorScheme.light().primary.withAlpha(blend),
                 const ColorScheme.light().surface),
           ),
+          useMaterial3: false,
         );
 
-    ThemeData darkTheme(int surfaceBlend, int backgroundBlend) =>
-        ThemeData.from(
+    ThemeData darkTheme(int blend) => ThemeData.from(
           colorScheme: ColorScheme.dark(
-            surface: Color.alphaBlend(
-                const ColorScheme.dark().primary.withAlpha(surfaceBlend),
+            surfaceContainer: Color.alphaBlend(
+                const ColorScheme.dark().primary.withAlpha(blend),
                 const ColorScheme.dark().surface),
           ),
+          useMaterial3: false,
         );
 
     testWidgets(
         'A) Expect no overlay in light ThemeData.from() with default '
-        'background and surface colors.', (WidgetTester tester) async {
-      // Default and equal surface and background colors.
-      final ThemeData light = lightTheme(0x00, 0x00);
-      final ThemeData dark = darkTheme(0x00, 0x00);
+        'container and surface colors.', (WidgetTester tester) async {
+      // Default and equal surface and container colors.
+      final ThemeData light = lightTheme(0x00);
+      final ThemeData dark = darkTheme(0x00);
 
       await tester.pumpWidget(testApp(ThemeMode.light, light, dark));
 
       final Color surface = Theme.of(savedContext).colorScheme.surface;
-      final Color background = Theme.of(savedContext).colorScheme.surface;
+      final Color container =
+          Theme.of(savedContext).colorScheme.surfaceContainer;
 
       // EXPECT: no elevation overlay color for none elevated surface.
       expect(surface,
@@ -65,26 +66,26 @@ void main() {
       expect(surface,
           equals(ElevationOverlay.applyOverlay(savedContext, surface, 1)));
 
-      // EXPECT: no elevation overlay color for none elevated background.
-      expect(background,
-          equals(ElevationOverlay.applyOverlay(savedContext, background, 0)));
-      // EXPECT: no elevation overlay color for elevated background.
-      expect(background,
-          equals(ElevationOverlay.applyOverlay(savedContext, background, 1)));
+      // EXPECT: no elevation overlay color for none elevated container.
+      expect(container,
+          equals(ElevationOverlay.applyOverlay(savedContext, container, 0)));
+      // EXPECT: no elevation overlay color for elevated container.
+      expect(container,
+          equals(ElevationOverlay.applyOverlay(savedContext, container, 1)));
     });
 
     testWidgets(
-        'B) Expect no overlay in light ThemeData.from() with background '
+        'B) Expect no overlay in light ThemeData.from() with container '
         'and surface colors using same none default color.',
         (WidgetTester tester) async {
-      // None default surface and background colors, but equal.
-      final ThemeData light = lightTheme(0x10, 0x10);
-      final ThemeData dark = darkTheme(0x10, 0x10);
+      // None default surface and container colors, but equal.
+      final ThemeData light = lightTheme(0x10);
+      final ThemeData dark = darkTheme(0x10);
 
       await tester.pumpWidget(testApp(ThemeMode.light, light, dark));
 
       final Color surface = Theme.of(savedContext).colorScheme.surface;
-      final Color background = Theme.of(savedContext).colorScheme.surface;
+      final Color container = Theme.of(savedContext).colorScheme.surface;
 
       // EXPECT: no elevation overlay color for none elevated surface.
       expect(surface,
@@ -93,26 +94,26 @@ void main() {
       expect(surface,
           equals(ElevationOverlay.applyOverlay(savedContext, surface, 1)));
 
-      // EXPECT: no elevation overlay color for none elevated background.
-      expect(background,
-          equals(ElevationOverlay.applyOverlay(savedContext, background, 0)));
-      // EXPECT: no elevation overlay color for elevated background.
-      expect(background,
-          equals(ElevationOverlay.applyOverlay(savedContext, background, 1)));
+      // EXPECT: no elevation overlay color for none elevated container.
+      expect(container,
+          equals(ElevationOverlay.applyOverlay(savedContext, container, 0)));
+      // EXPECT: no elevation overlay color for elevated container.
+      expect(container,
+          equals(ElevationOverlay.applyOverlay(savedContext, container, 1)));
     });
 
     testWidgets(
-        'C) Expect no overlay in light ThemeData.from() with background '
+        'C) Expect no overlay in light ThemeData.from() with container '
         'and surface colors using different none default color.',
         (WidgetTester tester) async {
-      // None default and not equal surface and background colors.
-      final ThemeData light = lightTheme(0x10, 0x20);
-      final ThemeData dark = darkTheme(0x10, 0x20);
+      // None default and not equal surface and container colors.
+      final ThemeData light = lightTheme(0x10);
+      final ThemeData dark = darkTheme(0x10);
 
       await tester.pumpWidget(testApp(ThemeMode.light, light, dark));
 
       final Color surface = Theme.of(savedContext).colorScheme.surface;
-      final Color background = Theme.of(savedContext).colorScheme.surface;
+      final Color container = Theme.of(savedContext).colorScheme.surface;
 
       // EXPECT: no elevation overlay color for none elevated surface.
       expect(surface,
@@ -121,25 +122,25 @@ void main() {
       expect(surface,
           equals(ElevationOverlay.applyOverlay(savedContext, surface, 1)));
 
-      // EXPECT: no elevation overlay color for none elevated background.
-      expect(background,
-          equals(ElevationOverlay.applyOverlay(savedContext, background, 0)));
-      // EXPECT: no elevation overlay color for elevated background.
-      expect(background,
-          equals(ElevationOverlay.applyOverlay(savedContext, background, 1)));
+      // EXPECT: no elevation overlay color for none elevated container.
+      expect(container,
+          equals(ElevationOverlay.applyOverlay(savedContext, container, 0)));
+      // EXPECT: no elevation overlay color for elevated container.
+      expect(container,
+          equals(ElevationOverlay.applyOverlay(savedContext, container, 1)));
     });
 
     testWidgets(
-        'D) Expect overlay in dark ThemeData.from() with default background '
+        'D) Expect overlay in dark ThemeData.from() with default container '
         'and surface colors, when elevation > 0.', (WidgetTester tester) async {
-      // Default and equal surface and background colors.
-      final ThemeData light = lightTheme(0x00, 0x00);
-      final ThemeData dark = darkTheme(0x00, 0x00);
+      // Default and equal surface and container colors.
+      final ThemeData light = lightTheme(0x00);
+      final ThemeData dark = darkTheme(0x00);
 
       await tester.pumpWidget(testApp(ThemeMode.dark, light, dark));
 
       final Color surface = Theme.of(savedContext).colorScheme.surface;
-      final Color background = Theme.of(savedContext).colorScheme.surface;
+      final Color container = Theme.of(savedContext).colorScheme.surface;
 
       // EXPECT: no elevation overlay color for none elevated surface.
       expect(surface,
@@ -148,26 +149,26 @@ void main() {
       expect(surface,
           isNot(ElevationOverlay.applyOverlay(savedContext, surface, 1)));
 
-      // EXPECT: no elevation overlay color for none elevated background.
-      expect(background,
-          equals(ElevationOverlay.applyOverlay(savedContext, background, 0)));
-      // EXPECT: elevation overlay color for elevated background.
-      expect(background,
-          isNot(ElevationOverlay.applyOverlay(savedContext, background, 1)));
+      // EXPECT: no elevation overlay color for none elevated container.
+      expect(container,
+          equals(ElevationOverlay.applyOverlay(savedContext, container, 0)));
+      // EXPECT: elevation overlay color for elevated container.
+      expect(container,
+          isNot(ElevationOverlay.applyOverlay(savedContext, container, 1)));
     });
 
     testWidgets(
-        'E) Expect overlay in dark ThemeData.from() with background and '
+        'E) Expect overlay in dark ThemeData.from() with container and '
         'surface colors using same none default color, when elevation > 0.',
         (WidgetTester tester) async {
-      // None default surface and background colors, but equal.
-      final ThemeData light = lightTheme(0x10, 0x10);
-      final ThemeData dark = darkTheme(0x10, 0x10);
+      // None default surface and container colors, but equal.
+      final ThemeData light = lightTheme(0x10);
+      final ThemeData dark = darkTheme(0x10);
 
       await tester.pumpWidget(testApp(ThemeMode.dark, light, dark));
 
       final Color surface = Theme.of(savedContext).colorScheme.surface;
-      final Color background = Theme.of(savedContext).colorScheme.surface;
+      final Color container = Theme.of(savedContext).colorScheme.surface;
 
       // EXPECT: no elevation overlay color for none elevated surface.
       expect(surface,
@@ -176,100 +177,76 @@ void main() {
       expect(surface,
           isNot(ElevationOverlay.applyOverlay(savedContext, surface, 1)));
 
-      // EXPECT: no elevation overlay color for none elevated background.
-      expect(background,
-          equals(ElevationOverlay.applyOverlay(savedContext, background, 0)));
-      // EXPECT: elevation overlay color for elevated background.
-      expect(background,
-          isNot(ElevationOverlay.applyOverlay(savedContext, background, 1)));
+      // EXPECT: no elevation overlay color for none elevated container.
+      expect(container,
+          equals(ElevationOverlay.applyOverlay(savedContext, container, 0)));
+      // EXPECT: elevation overlay color for elevated container.
+      expect(container,
+          isNot(ElevationOverlay.applyOverlay(savedContext, container, 1)));
     });
 
     testWidgets(
-        'F1) Expect NO overlay in dark ThemeData.from() on background and '
+        'F1) Expect NO overlay in dark ThemeData.from() on container and '
         'surface colors using separate and none default color, when '
         'elevation = 0.', (WidgetTester tester) async {
-      // None default and not equal surface and background colors.
-      final ThemeData light = lightTheme(0x10, 0x20);
-      final ThemeData dark = darkTheme(0x10, 0x20);
+      // None default and not equal surface and container colors.
+      final ThemeData light = lightTheme(0x10);
+      final ThemeData dark = darkTheme(0x10);
 
       await tester.pumpWidget(testApp(ThemeMode.dark, light, dark));
 
       final Color surface = Theme.of(savedContext).colorScheme.surface;
-      final Color background = Theme.of(savedContext).colorScheme.surface;
+      final Color container = Theme.of(savedContext).colorScheme.surface;
 
       // EXPECT: no elevation overlay color for none elevated surface.
       expect(surface,
           equals(ElevationOverlay.applyOverlay(savedContext, surface, 0)));
 
-      // EXPECT: no elevation overlay color for none elevated background.
-      expect(background,
-          equals(ElevationOverlay.applyOverlay(savedContext, background, 0)));
+      // EXPECT: no elevation overlay color for none elevated container.
+      expect(container,
+          equals(ElevationOverlay.applyOverlay(savedContext, container, 0)));
     });
 
     testWidgets(
-        'F2) Expect *NO* overlay in dark ThemeData.from() on background, when '
-        'different from surface color and using separate and none default '
+        'F2) Expect *NO* overlay in dark ThemeData.from() on surfaceContainer, '
+        'when different from surface color and using separate and none default '
         'color, when elevation > 0. '
         'THIS IS REVERSE EXPECTATION: '
         'WE WOULD EXPECT ELEVATION ON THIS AND THIS TEST TO FAIL!',
         (WidgetTester tester) async {
-      // None default and not equal surface and background colors.
-      final ThemeData light = lightTheme(0x10, 0x20);
-      final ThemeData dark = darkTheme(0x10, 0x20);
+      // None default and not equal surface and container colors.
+      final ThemeData light = lightTheme(0x10);
+      final ThemeData dark = darkTheme(0x10);
 
       await tester.pumpWidget(testApp(ThemeMode.dark, light, dark));
 
       final Color surface = Theme.of(savedContext).colorScheme.surface;
-      final Color background = Theme.of(savedContext).colorScheme.surface;
+      final Color container =
+          Theme.of(savedContext).colorScheme.surfaceContainer;
 
       // EXPECT: elevation overlay color for elevated surface.
       expect(surface,
           isNot(ElevationOverlay.applyOverlay(savedContext, surface, 1)));
 
-      // EXPECT: **NO** elevation overlay color for elevated background.
+      // EXPECT: **NO** elevation overlay color for elevated surface container.
       // We would ACTUALLY WANT  THIS TO FAIL, this is reverse failed test
       // Based on HOW SDK currently behaves.
       // See issue: https://github.com/flutter/flutter/issues/90353
-      expect(background,
-          equals(ElevationOverlay.applyOverlay(savedContext, background, 1)));
+      expect(container,
+          equals(ElevationOverlay.applyOverlay(savedContext, container, 1)));
     });
 
     testWidgets(
-        'G1) Expect **NO** overlay in dark ThemeData() on background and '
+        'G1) Expect **NO** overlay in dark ThemeData() on container and '
         'surface colors when using default ThemeData() factory with '
         'applyElevationOverlayColor = true and elevation = 0.',
         (WidgetTester tester) async {
       // Light and dark theme data made using
       // ThemeData(applyElevationOverlayColor: true) factory.
-      final ThemeData light = ThemeData(applyElevationOverlayColor: true);
-      final ThemeData dark = ThemeData(
-        brightness: Brightness.dark,
+      final ThemeData light = ThemeData(
         applyElevationOverlayColor: true,
+        useMaterial3: false,
       );
-
-      await tester.pumpWidget(testApp(ThemeMode.dark, light, dark));
-
-      final Color surface = Theme.of(savedContext).colorScheme.surface;
-      final Color background = Theme.of(savedContext).colorScheme.surface;
-
-      // EXPECT: no elevation overlay color for none elevated surface.
-      expect(surface,
-          equals(ElevationOverlay.applyOverlay(savedContext, surface, 0)));
-
-      // EXPECT: no elevation overlay color for none elevated background.
-      expect(background,
-          equals(ElevationOverlay.applyOverlay(savedContext, background, 0)));
-    });
-
-    testWidgets(
-        'G2) Expect *NO* overlay in dark ThemeData() on background, when '
-        'surface color when uses different color with default ThemeData() '
-        'factory with: applyElevationOverlayColor = true and elevation > 0. '
-        'THIS IS REVERSE EXPECTATION: WE WOULD EXPECT ELEVATION ON THIS '
-        'AND THIS TEST TO FAIL!', (WidgetTester tester) async {
-      // Light and dark theme data made using
-      // ThemeData(applyElevationOverlayColor: true) factory.
-      final ThemeData light = ThemeData(applyElevationOverlayColor: true);
       final ThemeData dark = ThemeData(
         brightness: Brightness.dark,
         applyElevationOverlayColor: true,
@@ -279,18 +256,55 @@ void main() {
       await tester.pumpWidget(testApp(ThemeMode.dark, light, dark));
 
       final Color surface = Theme.of(savedContext).colorScheme.surface;
-      final Color background = Theme.of(savedContext).colorScheme.surface;
+      final Color container = Theme.of(savedContext).colorScheme.surface;
+
+      // EXPECT: no elevation overlay color for none elevated surface.
+      expect(surface,
+          equals(ElevationOverlay.applyOverlay(savedContext, surface, 0)));
+
+      // EXPECT: no elevation overlay color for none elevated container.
+      expect(container,
+          equals(ElevationOverlay.applyOverlay(savedContext, container, 0)));
+    });
+
+    testWidgets(
+        'G2) Expect *NO* overlay in dark ThemeData() on container, when '
+        'surface color when uses different color with default ThemeData() '
+        'factory with: applyElevationOverlayColor = true and elevation > 0. '
+        'THIS IS REVERSE EXPECTATION: WE WOULD EXPECT ELEVATION ON THIS '
+        'AND THIS TEST TO FAIL!', (WidgetTester tester) async {
+      // Light and dark theme data made using
+      // ThemeData(applyElevationOverlayColor: true) factory.
+      final ThemeData light = ThemeData(
+        applyElevationOverlayColor: true,
+        useMaterial3: false,
+      );
+      ThemeData dark = ThemeData(
+        brightness: Brightness.dark,
+        applyElevationOverlayColor: true,
+        useMaterial3: false,
+      );
+      dark = dark.copyWith(
+          colorScheme: dark.colorScheme.copyWith(
+        surfaceContainer: Colors.green,
+      ));
+
+      await tester.pumpWidget(testApp(ThemeMode.dark, light, dark));
+
+      final Color surface = Theme.of(savedContext).colorScheme.surface;
+      final Color container =
+          Theme.of(savedContext).colorScheme.surfaceContainer;
 
       // EXPECT: elevation overlay color for elevated surface.
       expect(surface,
           isNot(ElevationOverlay.applyOverlay(savedContext, surface, 1)));
 
-      // EXPECT: **NO** elevation overlay color for elevated background.
+      // EXPECT: **NO** elevation overlay color for elevated container.
       // We would ACTUALLY WANT  THIS TO FAIL, this is reverse failed test
       // Based on HOW SDK currently behaves.
       // See issue: https://github.com/flutter/flutter/issues/90353
-      expect(background,
-          equals(ElevationOverlay.applyOverlay(savedContext, background, 1)));
+      expect(container,
+          equals(ElevationOverlay.applyOverlay(savedContext, container, 1)));
     });
   });
 }
