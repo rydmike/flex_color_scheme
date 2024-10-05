2ba63a0b7817bb314d3314993e856dbec4b01e87
Rydmike <39990307+rydmike@users.noreply.github.com>
2024-10-05 17:11:16 +0300

    Update: Embedders: Call the app Playground on platforms
    This also impacts all the other examples, but that is fine. Example 5, the Playground is the only one we are interested in building.

--- example/windows/runner/main.cpp
+++ example/windows/runner/main.cpp
@@ -26,8 +26,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 950);
-  if (!window.CreateAndShow(L"Themes Playground", origin, size)) {
+  Win32Window::Size size(1800, 970);
+  if (!window.Create(L"Playground", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
