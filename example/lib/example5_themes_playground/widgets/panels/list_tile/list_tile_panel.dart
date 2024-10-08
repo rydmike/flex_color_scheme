7d0a539637fb7e185d5a832b7df3c033fb941834
Rydmike <39990307+rydmike@users.noreply.github.com>
2024-10-08 20:57:43 +0300

    Refactor ListTile: Two column control layouts to ResponsiveTwoWidgets

--- example/lib/example5_themes_playground/widgets/panels/list_tile/list_tile_panel.dart
+++ example/lib/example5_themes_playground/widgets/panels/list_tile/list_tile_panel.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../../../../shared/controllers/theme_controller.dart';
 import '../../../../shared/utils/link_text_span.dart';
 import '../../../../shared/widgets/universal/list_tile_reveal.dart';
+import '../../../../shared/widgets/universal/responsive_two_widgets.dart';
 import '../../../../shared/widgets/universal/showcase_material.dart';
 import '../../../../shared/widgets/universal/slider_list_tile_reveal.dart';
 import '../../../theme/theme_values.dart';
@@ -40,204 +41,180 @@ class ListTilePanel extends StatelessWidget {
           value: controller.listTileSelectedSchemeColor,
           onChanged: controller.setListTileSelectedSchemeColor,
         ),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Expanded(
-              child: ColorSchemePopupMenu(
-                enabled: enableControl,
-                contentPadding: ThemeValues.tilePaddingStart(context),
-                title: const Text('Icon color'),
-                defaultLabel: 'onSurfaceVariant',
-                defaultLabelM2: 'Black opacity 54%',
-                defaultLabelDarkM2: 'White',
-                value: controller.listTileIconSchemeColor,
-                onChanged: controller.setListTileIconSchemeColor,
-              ),
+        ResponsiveTwoWidgets(builder: (BuildContext context, bool isRow) {
+          return RowOrColumn(
+            firstWidget: ColorSchemePopupMenu(
+              enabled: enableControl,
+              contentPadding: ThemeValues.tilePaddingStart(context, isRow),
+              title: const Text('Icon color'),
+              defaultLabel: 'onSurfaceVariant',
+              defaultLabelM2: 'Black opacity 54%',
+              defaultLabelDarkM2: 'White',
+              value: controller.listTileIconSchemeColor,
+              onChanged: controller.setListTileIconSchemeColor,
             ),
-            Expanded(
-              child: ColorSchemePopupMenu(
-                enabled: enableControl,
-                contentPadding: ThemeValues.tilePaddingEnd(context),
-                title: const Text('Text color'),
-                defaultLabel: 'Used text styles',
-                defaultLabelM2: 'Used text styles & subtitle bodySmall',
-                defaultDisabledLabel: 'onSurface',
-                value: controller.listTileTextSchemeColor,
-                onChanged: controller.setListTileTextSchemeColor,
-              ),
+            lastWidget: ColorSchemePopupMenu(
+              enabled: enableControl,
+              contentPadding: ThemeValues.tilePaddingEnd(context, isRow),
+              title: const Text('Text color'),
+              defaultLabel: 'Used text styles',
+              defaultLabelM2: 'Used text styles & subtitle bodySmall',
+              defaultDisabledLabel: 'onSurface',
+              value: controller.listTileTextSchemeColor,
+              onChanged: controller.setListTileTextSchemeColor,
             ),
-          ],
-        ),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Expanded(
-              child: ColorSchemePopupMenu(
-                enabled: enableControl,
-                contentPadding: ThemeValues.tilePaddingStart(context),
-                title: const Text('Tile color'),
-                defaultLabel: 'transparent',
-                value: controller.listTileTileSchemeColor,
-                onChanged: controller.setListTileTileSchemeColor,
-              ),
+            isRow: isRow,
+          );
+        }),
+        ResponsiveTwoWidgets(builder: (BuildContext context, bool isRow) {
+          return RowOrColumn(
+            firstWidget: ColorSchemePopupMenu(
+              enabled: enableControl,
+              contentPadding: ThemeValues.tilePaddingStart(context, isRow),
+              title: const Text('Tile color'),
+              defaultLabel: 'transparent',
+              value: controller.listTileTileSchemeColor,
+              onChanged: controller.setListTileTileSchemeColor,
             ),
-            Expanded(
-              child: ColorSchemePopupMenu(
-                enabled: enableControl,
-                contentPadding: ThemeValues.tilePaddingEnd(context),
-                title: const Text('Selected tile color'),
-                defaultLabel: 'transparent',
-                value: controller.listTileSelectedTileSchemeColor,
-                onChanged: controller.setListTileSelectedTileSchemeColor,
-              ),
+            lastWidget: ColorSchemePopupMenu(
+              enabled: enableControl,
+              contentPadding: ThemeValues.tilePaddingEnd(context, isRow),
+              title: const Text('Selected tile color'),
+              defaultLabel: 'transparent',
+              value: controller.listTileSelectedTileSchemeColor,
+              onChanged: controller.setListTileSelectedTileSchemeColor,
             ),
-          ],
-        ),
+            isRow: isRow,
+          );
+        }),
         ListTile(
           enabled: controller.useSubThemes && controller.useFlexColorScheme,
           title: const Text('Content padding'),
         ),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Expanded(
-              child: SliderListTileReveal(
-                contentPadding: ThemeValues.tilePaddingStart(context),
-                trailingWidth: 52,
-                enabled: enableControl,
-                value: controller.listTilePaddingStart,
-                onChanged: controller.setListTilePaddingStart,
-                min: 0,
-                max: 50,
-                divisions: 50,
-                valueDecimalPlaces: 0,
-                valueHeading: 'START',
-                valueUnitLabel: ' dp',
-                valueDefaultLabel: '16 dp',
-              ),
+        ResponsiveTwoWidgets(builder: (BuildContext context, bool isRow) {
+          return RowOrColumn(
+            firstWidget: SliderListTileReveal(
+              contentPadding: ThemeValues.tilePaddingStart(context, isRow),
+              trailingWidth: 52,
+              enabled: enableControl,
+              value: controller.listTilePaddingStart,
+              onChanged: controller.setListTilePaddingStart,
+              min: 0,
+              max: 50,
+              divisions: 50,
+              valueDecimalPlaces: 0,
+              valueHeading: 'START',
+              valueUnitLabel: ' dp',
+              valueDefaultLabel: '16 dp',
             ),
-            Expanded(
-              child: SliderListTileReveal(
-                contentPadding: ThemeValues.tilePaddingEnd(context),
-                trailingWidth: 60,
-                enabled: enableControl,
-                value: controller.listTilePaddingEnd,
-                onChanged: controller.setListTilePaddingEnd,
-                min: 0,
-                max: 50,
-                divisions: 50,
-                valueDecimalPlaces: 0,
-                valueHeading: 'END',
-                valueUnitLabel: ' dp',
-                valueDefaultLabel: useMaterial3 ? '24 dp' : '16 dp',
-              ),
+            lastWidget: SliderListTileReveal(
+              contentPadding: ThemeValues.tilePaddingEnd(context, isRow),
+              trailingWidth: 60,
+              enabled: enableControl,
+              value: controller.listTilePaddingEnd,
+              onChanged: controller.setListTilePaddingEnd,
+              min: 0,
+              max: 50,
+              divisions: 50,
+              valueDecimalPlaces: 0,
+              valueHeading: 'END',
+              valueUnitLabel: ' dp',
+              valueDefaultLabel: useMaterial3 ? '24 dp' : '16 dp',
             ),
-          ],
-        ),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Expanded(
-              child: SliderListTileReveal(
-                contentPadding: ThemeValues.tilePaddingStart(context),
-                trailingWidth: 52,
-                enabled: enableControl,
-                value: controller.listTilePaddingTop,
-                onChanged: controller.setListTilePaddingTop,
-                min: 0,
-                max: 50,
-                divisions: 50,
-                valueDecimalPlaces: 0,
-                valueHeading: 'TOP',
-                valueUnitLabel: ' dp',
-                valueDefaultLabel: '0 dp',
-              ),
+            isRow: isRow,
+          );
+        }),
+        ResponsiveTwoWidgets(builder: (BuildContext context, bool isRow) {
+          return RowOrColumn(
+            firstWidget: SliderListTileReveal(
+              contentPadding: ThemeValues.tilePaddingStart(context, isRow),
+              trailingWidth: 52,
+              enabled: enableControl,
+              value: controller.listTilePaddingTop,
+              onChanged: controller.setListTilePaddingTop,
+              min: 0,
+              max: 50,
+              divisions: 50,
+              valueDecimalPlaces: 0,
+              valueHeading: 'TOP',
+              valueUnitLabel: ' dp',
+              valueDefaultLabel: '0 dp',
             ),
-            Expanded(
-              child: SliderListTileReveal(
-                contentPadding: ThemeValues.tilePaddingEnd(context),
-                trailingWidth: 60,
-                enabled: enableControl,
-                value: controller.listTilePaddingBottom,
-                onChanged: controller.setListTilePaddingBottom,
-                min: 0,
-                max: 50,
-                divisions: 50,
-                valueDecimalPlaces: 0,
-                valueHeading: 'BOTTOM',
-                valueUnitLabel: ' dp',
-                valueDefaultLabel: '0 dp',
-              ),
+            lastWidget: SliderListTileReveal(
+              contentPadding: ThemeValues.tilePaddingEnd(context, isRow),
+              trailingWidth: 60,
+              enabled: enableControl,
+              value: controller.listTilePaddingBottom,
+              onChanged: controller.setListTilePaddingBottom,
+              min: 0,
+              max: 50,
+              divisions: 50,
+              valueDecimalPlaces: 0,
+              valueHeading: 'BOTTOM',
+              valueUnitLabel: ' dp',
+              valueDefaultLabel: '0 dp',
             ),
-          ],
-        ),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Expanded(
-              child: SliderListTileReveal(
-                title: const Text('Horizontal title gap'),
-                contentPadding: ThemeValues.tilePaddingStart(context),
-                trailingWidth: 52,
-                enabled: enableControl,
-                value: controller.listTileHorizontalTitleGap,
-                onChanged: controller.setListTileHorizontalTitleGap,
-                min: 0,
-                max: 50,
-                divisions: 50,
-                valueDecimalPlaces: 0,
-                valueHeading: 'GAP',
-                valueUnitLabel: ' dp',
-                valueDefaultLabel: '16 dp',
-              ),
+            isRow: isRow,
+          );
+        }),
+        ResponsiveTwoWidgets(builder: (BuildContext context, bool isRow) {
+          return RowOrColumn(
+            firstWidget: SliderListTileReveal(
+              title: const Text('Horizontal title gap'),
+              contentPadding: ThemeValues.tilePaddingStart(context, isRow),
+              trailingWidth: 52,
+              enabled: enableControl,
+              value: controller.listTileHorizontalTitleGap,
+              onChanged: controller.setListTileHorizontalTitleGap,
+              min: 0,
+              max: 50,
+              divisions: 50,
+              valueDecimalPlaces: 0,
+              valueHeading: 'GAP',
+              valueUnitLabel: ' dp',
+              valueDefaultLabel: '16 dp',
             ),
-            Expanded(
-              child: SliderListTileReveal(
-                title: const Text('Min vertical padding'),
-                contentPadding: ThemeValues.tilePaddingEnd(context),
-                trailingWidth: 60,
-                enabled: enableControl,
-                value: controller.listTileMinVerticalPadding,
-                onChanged: controller.setListTileMinVerticalPadding,
-                min: 0,
-                max: 50,
-                divisions: 50,
-                valueDecimalPlaces: 0,
-                valueHeading: 'PADDING',
-                valueUnitLabel: ' dp',
-                valueDefaultLabel: useMaterial3 ? '8 dp' : '4 dp',
-              ),
+            lastWidget: SliderListTileReveal(
+              title: const Text('Min vertical padding'),
+              contentPadding: ThemeValues.tilePaddingEnd(context, isRow),
+              trailingWidth: 60,
+              enabled: enableControl,
+              value: controller.listTileMinVerticalPadding,
+              onChanged: controller.setListTileMinVerticalPadding,
+              min: 0,
+              max: 50,
+              divisions: 50,
+              valueDecimalPlaces: 0,
+              valueHeading: 'PADDING',
+              valueUnitLabel: ' dp',
+              valueDefaultLabel: useMaterial3 ? '8 dp' : '4 dp',
             ),
-          ],
-        ),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Expanded(
-              child: EnumPopupMenu<ListTileStyle>(
-                contentPadding: ThemeValues.tilePaddingStart(context),
-                enabled: enableControl,
-                values: ListTileStyle.values,
-                title: const Text('Style'),
-                // defaultLabel: 'transparent',
-                value: controller.listTileStyle,
-                onChanged: controller.setListTileStyle,
-              ),
+            isRow: isRow,
+          );
+        }),
+        ResponsiveTwoWidgets(builder: (BuildContext context, bool isRow) {
+          return RowOrColumn(
+            firstWidget: EnumPopupMenu<ListTileStyle>(
+              contentPadding: ThemeValues.tilePaddingStart(context, isRow),
+              enabled: enableControl,
+              values: ListTileStyle.values,
+              title: const Text('Style'),
+              // defaultLabel: 'transparent',
+              value: controller.listTileStyle,
+              onChanged: controller.setListTileStyle,
             ),
-            Expanded(
-              child: EnumPopupMenu<ListTileTitleAlignment>(
-                contentPadding: ThemeValues.tilePaddingEnd(context),
-                enabled: enableControl,
-                values: ListTileTitleAlignment.values,
-                title: const Text('Title alignment'),
-                // defaultLabel: 'transparent',
-                value: controller.listTileTitleAlignment,
-                onChanged: controller.setListTileTitleAlignment,
-              ),
+            lastWidget: EnumPopupMenu<ListTileTitleAlignment>(
+              contentPadding: ThemeValues.tilePaddingEnd(context, isRow),
+              enabled: enableControl,
+              values: ListTileTitleAlignment.values,
+              title: const Text('Title alignment'),
+              // defaultLabel: 'transparent',
+              value: controller.listTileTitleAlignment,
+              onChanged: controller.setListTileTitleAlignment,
             ),
-          ],
-        ),
+            isRow: isRow,
+          );
+        }),
         ListTileReveal(
           dense: true,
           title: const Text('Known issues'),
