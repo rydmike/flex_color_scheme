a5d5c6e9876f0f6066aa22cd48a9462c91514d43
Rydmike <39990307+rydmike@users.noreply.github.com>
2024-10-14 10:26:10 +0300

    Bump: Versions to 8.0.0-dev.2 for QualityAssurance phase

--- example/lib/shared/const/app.dart
+++ example/lib/shared/const/app.dart
@@ -36,7 +36,7 @@ sealed class App {
   // build numbers.
   static const String versionMajor = '8';
   static const String versionMinor = '0';
-  static const String versionPatch = '0-dev.1';
+  static const String versionPatch = '0-dev.2';
   static const String versionBuild = '01';
   static const String versionFull = '$versionMajor.$versionMinor.$versionPatch'
       '\nBuild-$versionBuild';
