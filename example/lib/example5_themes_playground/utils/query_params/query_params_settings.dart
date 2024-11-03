8ed9671d39701628afd17f24c84ccc0fc0014cc3
rydmike <mrydstrom+gh@gmail.com>
2024-11-03 22:15:40 +0200

    Disable debugLog

--- example/lib/example5_themes_playground/utils/query_params/query_params_settings.dart
+++ example/lib/example5_themes_playground/utils/query_params/query_params_settings.dart
@@ -11,7 +11,7 @@ import 'query_params_handler.dart'
 // The handy part is that if it gets in the way in debugging, it is an easy
 // toggle to turn it off here too. Often I just leave them true if it is one
 // I want to see in dev mode, unless it is too chatty.
-const bool _debug = !kReleaseMode && true;
+const bool _debug = !kReleaseMode && false;
 
 /// A widget that rebuilds when query parameters change.
 ///
