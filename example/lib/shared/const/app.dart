2706a5ad4585cb7836e41258cd01fc62c8c0cd38
rydmike <mrydstrom+gh@gmail.com>
2025-08-15 15:40:41 +0300

    Playground: Bump version number

--- example/lib/shared/const/app.dart
+++ example/lib/shared/const/app.dart
@@ -41,15 +41,15 @@ abstract final class App {
     'dart.tool.dart2wasm',
   );
   static const String buildType = isRunningWithWasm
-      ? ', WasmGC'
+      ? 'WasmGC'
       : kIsWeb
-          ? ', JS'
-          : ', native VM';
+          ? 'JS'
+          : 'native VM';
   // Version of the WEB build, usually same as package, but it also has a
   // build numbers.
   static const String versionMajor = '8';
-  static const String versionMinor = '2';
-  static const String versionPatch = '1-dev.2';
+  static const String versionMinor = '3';
+  static const String versionPatch = '0';
   static const String versionBuild = '01';
   static const String versionFull = '$versionMajor.$versionMinor.$versionPatch'
       '\nBuild-$versionBuild';
