7d4ba8844f3bf909f6b4f2de53e8803915dd81da
rydmike <mrydstrom+gh@gmail.com>
2026-09-13 20:05:45 +0300

    chore: Update after build with Flutter 3.44.8

--- test/flex_color_scheme_to_theme_test.dart
+++ test/flex_color_scheme_to_theme_test.dart
@@ -2,6 +2,7 @@
 
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flex_color_scheme/src/flex_constants.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
